a848c627220bfacf665b5a53d7b1e07f7b1dce6f
Omar Said Kamel Ahmed <75030145+omarsaidkamel@users.noreply.github.com>
2025-03-13 13:40:11 +0200

    Update main.dart

--- Lecture1/main.dart
+++ Lecture1/main.dart
@@ -1,74 +1,65 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-List<String> images = ['assets/image.jpg','assets/image.jpg','assets/image.jpg','assets/image.jpg','assets/image.jpg'];
-List<String> firstname = ['omar','ahmed','samy','said','gamel'];
-List<String> describtion = ['assets/image.jpg','assets/image.jpg','assets/image.jpg','assets/image.jpg','assets/image.jpg'];
-List<String> time = ['10:10','02:06','15:09','01:02','11:03'];
-
+List<String> names = ['omar','samy','ahmed'];
+List<String> code = ['3131','4356','7664543'];
+List<String> time = ['12:03','05:06','05:09'];
 void main(){
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: ListView.builder(
-          itemCount: images.length,
-            itemBuilder: (context,index){
-              return Container(
-                  height: 120,
-                  padding: EdgeInsets.all(25),
+          itemCount:names.length,
+          itemBuilder: (context,index){
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  height: 80,
                   child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       CircleAvatar(
                         radius: 40,
-                        backgroundImage: AssetImage(images[index]),
+                        backgroundImage: AssetImage('assets/asd.jpg'),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10.0),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(top: 5.0,bottom: 10),
-                              child: Text(firstname[index],style: TextStyle(fontWeight: FontWeight.bold),),
-                            ),
-                            Text(describtion[index],style: TextStyle(fontSize: 13),),
-                          ],
-                        ),
+                      SizedBox(width: 20,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Name: ${names[index]}',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+                          SizedBox(height: 15,),
+                          Text('Code: ${code[index]}'),
+                        ],
                       ),
                       Spacer(),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: Row(
-                          children: [
-                            Icon(Icons.favorite_border_outlined),
-                            SizedBox(width: 15,),
-                            Text(time[index]),
-                          ],
-                        ),
-                      ),
+                      Icon(Icons.done_all),
+                      Text('${time[index]}'),
                     ],
                   ),
-                );
-            }
+                ),
+              ),
+              Container(
+                height: 1,
+                width: double.infinity,
+                color: Colors.grey,
+              ),
+            ],
+          );
+        },
+        ),
+        appBar: AppBar(
+          //toolbarHeight: 100,
+          leading: Icon(Icons.list,color: Colors.white,size: 40,),
+          title: Text('Your app',style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.deepPurple,
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: (){},
-          backgroundColor: Colors.purple,
-          child: Icon(Icons.add,color: Colors.white,size: 35,),
-        ),
-        drawer: Drawer(),
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-         // centerTitle: true,
-          backgroundColor: Colors.purple,
-          title: Text(
-              'Your App',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
+          backgroundColor: Colors.deepPurple,
+          child: Icon(Icons.add,color: Colors.white,),
         ),
       ),
-    ),
+    )
   );
 }
