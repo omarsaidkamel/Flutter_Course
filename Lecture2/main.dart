a2595ac2c5a4ebf43ad30ef998948d3ae3e0fd2d
Omar Said Kamel Ahmed <75030145+omarsaidkamel@users.noreply.github.com>
2025-03-18 00:17:25 +0200

    Create main.dart

---
+++ Lecture2/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/firstscreen.dart';
+
+
+void main(){
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Firstscreen(),
+      )
+  );
+}
