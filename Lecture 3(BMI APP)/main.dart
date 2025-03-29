3bd8aee2971c2d6103b8d988c3d2d36b16539ceb
Omar Said Kamel Ahmed <75030145+omarsaidkamel@users.noreply.github.com>
2025-03-29 03:05:53 +0200

    Create main.dart

---
+++ Lecture 3(BMI APP)/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'bmi_screen.dart';
+import 'bmiresult.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+       debugShowCheckedModeBanner: false,
+      home: bmiscreen(),
+    ),
+  );
+}
